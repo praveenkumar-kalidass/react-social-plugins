d9d92c6eac16d2859d6b1a1fcdc83a942525a416
praveenkumar-outlook <praveen.ece@outlook.com>
2018-09-06 15:55:01 +0530

    Webpack : Dev config

---
+++ dev/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.join(__dirname, 'app/app.js'),
+  output: {
+    path: path.join(__dirname, '../public'),
+    filename: 'app.bundle.js'
+  },
+  mode: 'development',
+  resolve: {
+    extensions: ['.js']
+  },
+  plugins: [new HtmlWebpackPlugin({
+    filename: path.join(__dirname, '../public/index.html'),
+    template: path.join(__dirname, 'index.html')
+  }),
+  new webpack.HotModuleReplacementPlugin()],  
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, '../public'),
+    open: true,
+    port: 3000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            babelrc: false,
+            presets: [
+              'react'
+            ]
+          }
+        }
+      }
+    ]
+  }
+};
